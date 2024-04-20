71718e0b22fcc90d8b5904f36801a8dad5f4a229
cliford-dareus <cliforddareus@gmail.com>
2024-04-19 23:45:02 -0400

    Fix ai instruction and style ai chat page

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -9,51 +9,94 @@ export async function POST(request: NextRequest) {
   const model = genAi.getGenerativeModel({
     model: "gemini-1.5-pro-latest",
     systemInstruction: {
-      role: "system",
+      role: "Teacher",
       parts: [
         {
           text: `
-          You are an English teacher, who shows your students an image of nature, 
-          and ask five questions but wait for a response after each question, give the student feedback on their correctness. 
-          Your questions should be short.
+            You are an English teacher. Give your students a quiz about animals.
+            Choose one of the image from the list based on it's description ask simple, distinct and interesting questions about it.
+            Ask a one question and wait for the student to answer then ask another. 
+            After one or two questions about an image choose a different image to ask questions about.
 
-          Question: question?,
-          feedback: feedback!,
-          wrong_answer:[wrong answer 1, wrong answer 2, wrong answer 3], 
-          correct_answer: the correct answer to your question,
-          image: [image url example: https://duckduckgo.com/?q=rhinoceros&t=h_&iax=images&ia=images&iai=https%3A%2F%2Fcdn.britannica.com%2F99%2F92699-050-26972485%2FWhite-rhinoceros-Mkuze-Game-Reserve-South-Africa.jpg].
+            Example:
+            Question: question?,
+            wrong_answer:[wrong answer 1, wrong answer 2, wrong answer 3], 
+            correct_answer: the correct answer to your question,
+            image: image url example: https://duckduckgo.com/?q=rhinoceros&t=h_&iax=images&ia=images&iai=https%3A%2F%2Fcdn.britannica.com%2F99%2F92699-050-26972485%2FWhite-rhinoceros-Mkuze-Game-Reserve-South-Africa.jpg.
 
-          '''''
-          rules:
-            if the student give a wrong answer ask the same question again. 
-            if the student give the correct answer ask a different question about the same image up to five questions.
-            then change to a new image and repeat.
-            don 't forget to give the student feedback on their correctness. 
-          '''''
 
-          '''''
-          Questions example:
-          What is the name of <the image>?
-          What is the color of <the image>?
-          What is the shape of <the image>?
-          what is the <image> wearing? 
-          where is it from?
-          what country is it from?
-          how many legs does it have?
-          how many eyes does it have?
-          how does it move around?
-          '''''
-          
-          Make sure the image you choose come from DuckDuckGo and take the first image from the search result.`
+          Choose image from this list: 
+          [
+            {
+              url: https://images.pexels.com/photos/1661179/pexels-photo-1661179.jpeg, 
+              description: Green Perched Parakeet sitting on a branch
+            },
+            {
+              url: https://images.pexels.com/photos/67552/giraffe-tall-mammal-africa-67552.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1, 
+              description: A Brown Giraffe from Africa Walking on Brown Grass, it is very tall 
+            },
+            {
+              url: https://images.pexels.com/photos/162140/duckling-birds-yellow-fluffy-162140.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1,
+              description: Duckling with yellow fllufy feathers on Black Soil during Daytime
+            },
+            {
+              url: https://images.pexels.com/photos/17811/pexels-photo.jpg,
+              description: Black and Green Toucan on Tree Branch
+            },
+            {
+              url: https://images.pexels.com/photos/219906/pexels-photo-219906.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1,
+              description: A Brown Deer Near Withered Tree with a blue bird sitting on its horn.
+            },
+            {
+              url: https://images.pexels.com/photos/162203/panthera-tigris-altaica-tiger-siberian-amurtiger-162203.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1,
+              description: Tiger Standing on the Grey Concrete Pavement
+            },
+            {
+              url: https://images.pexels.com/photos/1851164/pexels-photo-1851164.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1,
+              description:  an Adult Black Pug
+            },
+            {
+              url: https://images.pexels.com/photos/3551498/pexels-photo-3551498.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1, 
+              description: Two Gray Elephants on the forest. 
+            },
+            {
+              url: https://images.pexels.com/photos/460961/pexels-photo-460961.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1,
+              description:  Brown and Black Bee on Yellow Flower
+            },
+            {
+              url: https://images.pexels.com/photos/750539/pexels-photo-750539.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1,
+              description:  Zebras Standing on an Open Field
+            },
+            {
+              url: https://images.pexels.com/photos/36762/scarlet-honeyeater-bird-red-feathers.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1,
+              description: Red and Black feathers Bird, called scarlet Honeyeater standing on Red Flowers.
+            },
+            {
+              url: https://images.pexels.com/photos/1575857/pexels-photo-1575857.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1,
+              description: Brown Llama Standing on outside in the snow
+            },
+          ]
+          `,
         },
       ],
     },
   });
 
-  const chat = model.startChat();
+  const chat = model.startChat({
+    history: [
+      {
+        role: "user",
+        parts: [{ text: "Hello!" }],
+      },
+      {
+        role: "model",
+        parts: [{ text: "Good morning! Let's start with quiz" }],
+      },
+    ],
+  });
 
   const result = await chat.sendMessage(prompt);
-  const response = await result.response;
+  const response = result.response;
   const text = response.text();
 
   return NextResponse.json({ succes: true, text: text });
--- src/app/chat/[slug]/page.tsx
+++ src/app/chat/[slug]/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import { getData } from "@/_actions/chat";
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 
 type Props = {
   params: {
@@ -27,58 +28,74 @@ type ResponseType = {
 };
 
 type InitialType = {
-  question: string;
-  feedback: string;
-  wrongAnswer: string[];
-  correctAnswer: string;
+  Question: string;
+  wrong_answer: string[];
+  correct_answer: string;
   image: string;
 };
 
 const initialQuestion = {
-  question: "",
-  feedback: "",
-  wrongAnswer: [],
-  correctAnswer: "",
+  Question: "",
+  wrong_answer: [],
+  correct_answer: "",
   image: "",
 } as InitialType;
 
 const Chat = ({ params }: Props) => {
   const [state, setState] = useState(initialQuestion);
   const [data, setData] = React.useState<ResponseType | null>(null);
-  const [imageUrl, setImageUrl] = React.useState("");
   const [formData, setFormData] = React.useState<FormDataType>(initialFormData);
 
   const handleChat = async (e: React.FormEvent) => {
     e.preventDefault();
     const data = await getData(formData);
-    console.log(data);
     setData(data);
   };
 
   const formatText = () => {
     if (!data) return;
-    const iFeedback = data.text.indexOf("feedback:");
-    const iQuestion = data.text.indexOf("Question:"); 
-    const iImage = data.text.indexOf("image:");
-    const iWrongAnswer = data.text.indexOf("wrong_answer:");
-    const iCorrectAnswer = data.text.indexOf("correct_answer:");
-
-    const question = data?.text?.slice(iQuestion, iFeedback).trim();
-    const feedback = data?.text?.slice(iFeedback, iWrongAnswer).trim();
-    const wrongAnswer = data?.text?.slice(iWrongAnswer, iCorrectAnswer).trim().split(',');
-    const correctAnswer = data?.text?.slice(iCorrectAnswer, iImage).trim();
-    const image = data?.text?.split("image:")[1].trim()
+    const inputs = data.text.split("\n");
 
     const newData = {
-      question,
-      feedback,
-      wrongAnswer,
-      correctAnswer,
-      image,
-    } as InitialType;
+      Question: "",
+      wrong_answer: [],
+      correct_answer: "",
+      image: "",
+    } as {
+      [key: string]: any;
+    };
+
+    for (const input of inputs) {
+      const trimInput = input.trim();
+      let identifier = trimInput.split(":")[0];
+
+      if (identifier === "wrong_answer") {
+        const wrongAnswersMatch = trimInput.match(/wrong_answer:\[(.*?)\]/);
+        const wrongAnswers = wrongAnswersMatch
+          ? wrongAnswersMatch[1].split(", ")
+          : [];
+        newData[identifier] = wrongAnswers;
+      } else if (identifier === "image") {
+        const url = trimInput.slice(trimInput.indexOf("image:") + 6)?.trim();
+        newData[identifier] =
+          url[url.length - 1] === "." ? url.slice(0, -1) : url;
+      } else if (identifier === "correct_answer") {
+        newData[identifier] = trimInput.split(":")[1].slice(0, -1)?.trim();
+      } else {
+        newData[identifier] = trimInput.split(":")[1]?.trim();
+      }
+    }
 
     setState((prev) => ({ ...prev, ...newData }));
   };
+
+  const shuffle =useCallback( (array: string[]) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array.sort(() => Math.random() - 0.5);
+  }, [state]);
 
   React.useEffect(() => {
     formatText();
@@ -86,24 +103,34 @@ const Chat = ({ params }: Props) => {
 
   return (
     <div className="h-full p-4">
-      <div className="h-[50vh] bg-slate-200 rounded-lg">{/* <Image /> */}
-        <img src={state.image} />
+      <div className="h-[40vh] relative bg-slate-200 rounded-lg">
+        <img src={state.image} className="w-full h-full absolute inset-0" />
       </div>
       <div className="mt-4">
         <div className="min-h-[50px] bg-slate-200 rounded-lg">
-          {state.feedback}
-          {state.question}
-          {state.wrongAnswer}
-          {state.correctAnswer}
+          {state.Question}
         </div>
-        <form action="" onSubmit={handleChat}>
+        <div className="mt-4 grid grid-cols-2 gap-4">
+          {shuffle([...state.wrong_answer, state.correct_answer]).map(
+            (item) => (
+              <Button key={item}>{item}</Button>
+            )
+          )}
+        </div>
+        <form
+          className="flex flex-col gap-4 mt-4"
+          action=""
+          onSubmit={handleChat}
+        >
           <Input
             value={formData.prompt}
             onChange={(e) =>
               setFormData({ ...formData, prompt: e.target.value })
             }
           />
-          <button type="submit">Submit</button>
+          <Button disabled={formData.prompt === ""} type="submit">
+            Submit
+          </Button>
         </form>
       </div>
     </div>
