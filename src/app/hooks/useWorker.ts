7f411f4ca24184c2f8de660e2809d16fb19adb99
cliford-dareus <cliforddareus@gmail.com>
2024-09-08 21:00:37 -0400

    Try to Fix worker issue again

--- src/app/hooks/useWorker.ts
+++ src/app/hooks/useWorker.ts
@@ -1,22 +1,26 @@
-"use client"
+"use client";
 
 import { useState } from "react";
 
 export interface MessageEventHandler {
-    (event: MessageEvent): void;
+  (event: MessageEvent): void;
 }
 
 export function useWorker(messageEventHandler: MessageEventHandler): Worker {
-    // Create new worker once and never again
-    const [worker] = useState(() => createWorker(messageEventHandler));
-    return worker;
+  // Create new worker once and never again
+  const [worker] = useState(() => createWorker(messageEventHandler));
+  return worker;
 }
 
 function createWorker(messageEventHandler: MessageEventHandler): Worker {
-    const worker = new Worker(new URL("../worker.js", import.meta.url), {
-        type: "module",
+  let worker: Worker | null = null;
+
+  if (typeof Worker !== "undefined") {
+    worker = new Worker(new URL("../worker.js", import.meta.url), {
+      type: "module",
     });
     // Listen for messages from the Web Worker
     worker.addEventListener("message", messageEventHandler);
-    return worker;
+  }
+  return worker!;
 }
