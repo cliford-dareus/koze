36b0ac143ff76bc035873eb84f1a6c16f21a387b
cliford-dareus <cliforddareus@gmail.com>
2024-09-11 14:59:48 -0400

    set up camera component

--- src/app/(pages)/translation/_components/camera-component.tsx
+++ src/app/(pages)/translation/_components/camera-component.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
+import { useCallback, useEffect, useRef, useState } from "react";
+import Image from "next/image";
+import Webcam from "react-webcam";
 import {
   Drawer,
   DrawerContent,
@@ -8,15 +10,21 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from "@/components/ui/drawer";
-import { LucideCamera } from "lucide-react";
-import { LegacyRef, useCallback, useEffect, useRef, useState } from "react";
-import Image from "next/image";
-import Webcam from "react-webcam";
+import { LucideBlend, LucideCamera, LucideContrast } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { createWorker } from "tesseract.js";
+import useImageProcessor from "@/app/hooks/useImageProcessor";
 
 const CameraComponent = () => {
+  const { preprocessImage } = useImageProcessor();
+  const [open, setIsOpen] = useState(false);
   const [imageCaptured, setImageCaptured] = useState("");
   const webcamRef = useRef<Webcam | null>(null);
+  const imageRef = useRef<HTMLImageElement>(null);
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const [imageContext, setImageContext] =
+    useState<CanvasRenderingContext2D | null>(null);
+  const [extractedData, setExtractedText] = useState("");
   const [cameraOptions, setCameraOptions] = useState({
     facingMode: "user",
     width: 1280,
@@ -28,19 +36,45 @@ const CameraComponent = () => {
     setImageCaptured(
       "https://images.pexels.com/photos/1228483/pexels-photo-1228483.jpeg",
     );
-
-    if (!canvasRef.current) {
-      const canvas = canvasRef.current!;
-    }
   }, [webcamRef]);
 
-  useEffect(() => {
-    if (canvasRef.current) {
+  const extractText = async () => {
+    setTimeout(() => {
+      if (!imageContext) return;
+      imageContext.putImageData(preprocessImage(canvasRef.current, ""), 0, 0);
+    }, 200);
+    const dataUrl = canvasRef.current?.toDataURL("image/jpeg");
+
+    try {
+      const worker = await createWorker("eng");
+      // await worker?.loadLanguage('eng');
+      // await worker?.initialize('eng');
+      const {
+        data
+      } = await worker.recognize(imageRef.current!);
+      // setExtractedText(data);
+      console.log(data);
+      await worker.terminate();
+    } catch (error) {
+      console.error("Error extracting text:", error);
+      setExtractedText("Error extracting text. Please try again.");
     }
-  }, []);
+  };
+
+  useEffect(() => {
+    if (!canvasRef.current || !imageRef.current) return;
+    imageRef.current.onload = () => {
+      console.log("LAODED");
+      const canvas = canvasRef.current!;
+      const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+      setImageContext(ctx);
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.drawImage(imageRef.current!, 0, 0);
+    };
+  }, [imageCaptured]);
 
   return (
-    <Drawer>
+    <Drawer open={open} onOpenChange={setIsOpen}>
       <DrawerTrigger className="">
         <LucideCamera size={20} />
       </DrawerTrigger>
@@ -64,16 +98,36 @@ const CameraComponent = () => {
               <Button onClick={capture}>Capture</Button>
             </>
           ) : (
-            <div className="relative w-full h-full">
-              <div className="absolute">
-                <div>B/W</div>
+            <div className="relative w-full h-full bg-black rounded-md">
+              <div className="absolute top-4 left-4 flex gap-4">
+                <div className="rounded-full w-[30px] h-[30px] bg-slate-700 flex justify-center items-center">
+                  <LucideContrast size={20} />
+                </div>
+                <div className="rounded-full w-[30px] h-[30px] bg-slate-700 flex justify-center items-center">
+                  <LucideBlend size={20} />
+                </div>
               </div>
 
-              <canvas ref={canvasRef} />
+              <canvas ref={canvasRef} className="w-full h-full hidden" />
+
+              <Image
+                className="w-full h-full object-contain"
+                src={imageCaptured}
+                alt=""
+                width={370}
+                height={720}
+                ref={imageRef}
+              />
 
               <div className="flex gap-4 justify-center items-center mt-4 rounded-full">
                 <Button onClick={() => setImageCaptured("")}>Retake</Button>
-                <Button>Extract</Button>
+                <Button
+                  onClick={() => {
+                    extractText();
+                  }}
+                >
+                  Extract
+                </Button>
               </div>
             </div>
           )}
--- src/app/(pages)/translation/page.tsx
+++ src/app/(pages)/translation/page.tsx
@@ -1,18 +1,3 @@
-"use client";
-
-import { translate } from "@/_actions/translate";
-import TextToSpeechButton from "@/components/text-to-speech-button";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectTrigger,
-} from "@/components/ui/select";
-import { Loader, LucideArrowLeftRight, LucidePlayCircle } from "lucide-react";
-import React, { useState } from "react";
 import TranslationMananger from "./_components/translation-manager";
 
 type Props = {};
